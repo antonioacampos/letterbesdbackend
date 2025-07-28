dd48a39d68bce3fe5ad5cb4d472ba6364367b87c
Antonio Campos <129632162+antonioacampos@users.noreply.github.com>
2025-07-28 17:36:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -260,4 +260,5 @@ def obter_recomendacoes(usuario):
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
